31940089938d78ef6fc92b9c4d72f3c605da2ab6
esha995 <eshaaaagarwal996@gmail.com>
2023-11-06 16:24:08 +0530

    commit3

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 export default function Netflix() {
+
+  const[ isScrolled,setIsScrolled,] = userState(false);
+  window.onscroll = ()=>{
+    setIsScrolled(window.pageYOffset===0? false:true);
+    return()=>( window.onscroll= null);
+  };
   return (
     <div>
-      
+      <h1>hachgjk</h1>
+      <button> gihgytfkuj</button>
+      <Navbar isScrolled={isScrolled}/>
     </div>
   )
 }
